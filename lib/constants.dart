8ca959a09c1a27e815b0d56f230c34a625b774f2
azix khan <azixkhan.55@gmail.com>
2024-07-19 22:41:10 +0500

    Update .gitignore to exclude lib/constants/constants.dart

--- lib/constants.dart
+++
@@ -1,4 +0,0 @@
-const String stripePublishableKey =
-    "pk_test_51PeFDzHW0gdAiQUpZy9LnaebwTVnKMh1EkIGFIsZoaDYUa3WowTg55FakQq1gjyzUm3qUW2apJTVrKVqrxlmcjIK00wNXNX1wT";
-const String stripeSecretKey =
-    "sk_test_51PeFDzHW0gdAiQUp4x06YAdyZUsX626IwTAs4IlH4RDzdo733X8gdiQ9seCQKpeTxxTpFFa05YGotEHSGN8cOtAM00d59lfUdP";
